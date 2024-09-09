e5998aab53f11c7ddb5a21c13842408c19e9ceb9
Jorge Souza <121060792+jorgedss@users.noreply.github.com>
2024-09-09 11:34:31 -0300

    Teste unitário: Sign In

--- src/components/nav-link.spec.tsx
+++ src/components/nav-link.spec.tsx
@@ -3,6 +3,8 @@ import { MemoryRouter } from 'react-router-dom'
 
 import { NavLink } from './nav-link'
 
+// NavLink depende do Router para funcionar, isso exige um navegador para trabalhar com as rotas. Mas, não é possível aqui nesse ambiente de teste. Por isso, usamos o segundo argumento na callback do describe. O componente MemoryRouter memoriza a rota ativa em memoria.
+
 describe('NavLink', () => {
   it('Should highlight the nav link when is the current page link', () => {
     const wrapper = render(
